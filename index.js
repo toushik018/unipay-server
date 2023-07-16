348711871d033fe17c2e9e1ba0283ce54e437781
toushik018 <tayebhossain018@gmail.com>
2023-07-16 10:10:03 +0600

    server with payment

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const cors = require("cors");
-const { ObjectId } = require("mongodb");
+const SSLCommerzPayment = require('sslcommerz-lts');
+const { ObjectId } = require('mongodb');
 require("dotenv").config();
 const app = express();
 
 const port = process.env.PORT || 5000;
 
-// Midleware
+// Middleware
 app.use(cors());
 app.use(express.json());
 
@@ -22,13 +23,62 @@ const client = new MongoClient(uri, {
   },
 });
 
+const store_id = process.env.SSL_store_id;
+const store_passwd = process.env.SSL_store_pass;
+const is_live = false; // true for live, false for sandbox
+
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    // Connect the client to the server (optional starting in v4.7)
     await client.connect();
 
     const toursCollection = client.db("toursDB").collection("tours");
     const clubsCollection = client.db("toursDB").collection("clubs");
+    const usersCollection = client.db("toursDB").collection("users");
+    const orderCollection = client.db("toursDB").collection("order");
+    const tourOrdersCollection = client.db("toursDB").collection("tourOrders");
+
+    // User APIs
+
+    app.get('/users', async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email, id: user.id };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: 'User already exists' });
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    // Admin APIs
+
+    app.get('/users/admin/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const result = { admin: user?.role === 'admin' };
+      res.send(result);
+    });
+
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        }
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    // Tour APIs
 
     app.get("/tours", async (req, res) => {
       const result = await toursCollection.find().toArray();
@@ -37,12 +87,11 @@ async function run() {
 
     app.get("/tours/:id", async (req, res) => {
       const id = req.params.id;
-      console.log(id);
       const query = { _id: new ObjectId(id) };
-      console.log(query);
       const result = await toursCollection.findOne(query);
       res.send(result);
     });
+
 
 
     app.post("/tours", async (req, res) => {
@@ -51,15 +100,211 @@ async function run() {
       res.send(result);
     });
 
+    // Club APIs
+
     app.post("/clubs", async (req, res) => {
       const club = req.body;
       const result = await clubsCollection.insertOne(club);
       res.send(result);
     });
 
-
     app.get("/clubs", async (req, res) => {
       const result = await clubsCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/clubs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await clubsCollection.findOne(query);
+      res.send(result);
+    });
+
+
+
+
+
+    // -------------------------   Payment APIs - Clubs -------------
+
+
+
+    const tran_id = new ObjectId().toString();
+
+    app.post("/order", async (req, res) => {
+      const { clubId } = req.body;
+      const club = await clubsCollection.findOne({ _id: new ObjectId(clubId) });
+      const order = req.body;
+
+      const data = {
+        total_amount: club?.fee,
+        currency: order.currency,
+        tran_id: tran_id, // use unique tran_id for each api call
+        success_url: `http://localhost:5000/payment/success/${tran_id}`,
+        fail_url: `http://localhost:5000/payment/fail/${tran_id}`,
+        cancel_url: 'http://localhost:3030/cancel',
+        ipn_url: 'http://localhost:3030/ipn',
+        shipping_method: 'Courier',
+        product_name: club?.name,
+        product_category: 'Electronic',
+        product_profile: 'general',
+        cus_name: order.name,
+        cus_email: order.email,
+        cus_add1: 'Dhaka',
+        cus_add2: 'Dhaka',
+        cus_city: 'Dhaka',
+        cus_state: 'Dhaka',
+        cus_postcode: '1000',
+        cus_country: 'Bangladesh',
+        cus_phone: order.phoneNumber,
+        cus_fax: '01711111111',
+        ship_name: 'Customer Name',
+        ship_add1: 'Dhaka',
+        ship_add2: 'Dhaka',
+        ship_city: 'Dhaka',
+        ship_state: 'Dhaka',
+        ship_postcode: 1000,
+        ship_country: 'Bangladesh',
+        order_date: order.date,
+        stu_batch: order.batch,
+        stu_id: order.id
+      };
+
+      const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live);
+      sslcz.init(data).then(apiResponse => {
+        // Redirect the user to the payment gateway
+        let GatewayPageURL = apiResponse.GatewayPageURL;
+        res.send({ url: GatewayPageURL });
+
+        const finalOrder = {
+          club,
+          order,
+          paidStatus: false,
+          transactionId: tran_id
+        };
+        const result = orderCollection.insertOne(finalOrder);
+
+        console.log('Redirecting to: ', GatewayPageURL);
+      });
+
+
+      app.post("/payment/success/:tranId", async (req, res) => {
+        console.log(req.params.tranId);
+        const result = await orderCollection.updateOne({ transactionId: req.params.tranId }, {
+          $set: {
+            paidStatus: true
+          }
+        });
+        if (result.modifiedCount > 0) {
+          res.redirect(`http://localhost:5173/payment/success/${req.params.tranId}`);
+        }
+      });
+
+      app.post("/payment/fail/:tranId", async (req, res) => {
+        const  result = await orderCollection.deleteOne({transactionId: req.params.tranId});
+
+        if(result.deletedCount){
+          res.redirect(`http://localhost:5173/payment/fail/${req.params.tranId}`)
+        }
+
+
+      })
+
+
+    });
+
+    app.get("/orders", async (req, res) => {
+      const result = await orderCollection.find().toArray();
+      res.send(result);
+    });
+
+
+
+
+   // -------------------------   Payment APIs - Tours -------------
+
+
+
+
+    app.post("/tourorders", async (req, res) => {
+      const { tourId } = req.body;
+      const tour = await toursCollection.findOne({ _id: new ObjectId(tourId) });
+      const order = req.body;
+
+      const data = {
+        total_amount: tour?.cost,
+        currency: order.currency,
+        tran_id: tran_id, // use unique tran_id for each api call
+        success_url: `http://localhost:5000/payment/success/${tran_id}`,
+        fail_url: `http://localhost:5000/payment/fail/${tran_id}`,
+        cancel_url: 'http://localhost:3030/cancel',
+        ipn_url: 'http://localhost:3030/ipn',
+        shipping_method: 'Courier',
+        product_name: tour?.destination,
+        product_category: 'Electronic',
+        product_profile: 'general',
+        cus_name: order.name,
+        cus_email: order.email,
+        cus_add1: 'Dhaka',
+        cus_add2: 'Dhaka',
+        cus_city: 'Dhaka',
+        cus_state: 'Dhaka',
+        cus_postcode: '1000',
+        cus_country: 'Bangladesh',
+        cus_phone: order.phoneNumber,
+        cus_fax: '01711111111',
+        ship_name: 'Customer Name',
+        ship_add1: 'Dhaka',
+        ship_add2: 'Dhaka',
+        ship_city: 'Dhaka',
+        ship_state: 'Dhaka',
+        ship_postcode: 1000,
+        ship_country: 'Bangladesh',
+        order_date: order.date,
+        stu_batch: order.batch,
+        stu_id: order.id
+      };
+
+      const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live);
+      sslcz.init(data).then(apiResponse => {
+        // Redirect the user to the payment gateway
+        let GatewayPageURL = apiResponse.GatewayPageURL;
+        res.send({ url: GatewayPageURL });
+
+        const finalOrder = {
+          tour,
+          order,
+          paidStatus: false,
+          transactionId: tran_id
+        };
+        const result = tourOrdersCollection.insertOne(finalOrder);
+
+        console.log('Redirecting to: ', GatewayPageURL);
+      });
+
+
+      app.post("/payment/success/:tranId", async (req, res) => {
+        console.log(req.params.tranId);
+        const result = await tourOrdersCollection.updateOne({ transactionId: req.params.tranId }, {
+          $set: {
+            paidStatus: true
+          }
+        });
+        if (result.modifiedCount > 0) {
+          res.redirect(`http://localhost:5173/payment/success/${req.params.tranId}`);
+        }
+      });
+
+      app.post("/payment/fail/:tranId", async (req, res) => {
+        const  result = await tourOrdersCollection.deleteOne({transactionId: req.params.tranId});
+
+        if(result.deletedCount){
+          res.redirect(`http://localhost:5173/payment/fail/${req.params.tranId}`)
+        }
+      })
+    });
+
+    app.get("/tourorders", async (req, res) => {
+      const result = await tourOrdersCollection.find().toArray();
       res.send(result);
     });
 
@@ -73,6 +318,7 @@ async function run() {
     // await client.close();
   }
 }
+
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
